c67d4d7964ee400dc4d9b55f493e16d6399183c3
Wei Li Wang <weili.wang123@gmail.com>
2015-11-01 08:18:40 -0800

    added mysqldb support

--- index.py
+++ index.py
@@ -1,10 +1,20 @@
 from paste import request
-import re
+import re, sys
+sys.path.append('/var/www/stock_program')
+import add.add as add
+
 def application(environ, start_response):
 	status = '200 OK'
-	path = re.sub("[^a-z]","",environ.get('PATH_INFO'))
-	query = dict(request.parse_querystring(environ))
-	output = path
+	output = ''
+	#get query
+	query = re.sub("[^a-z]","",environ.get('PATH_INFO'))
+	#get arguments
+	arguments = dict(request.parse_querystring(environ))
+
+	#add stock to database
+	if query == 'add':
+		#file that handles adding stock to database
+		output = add.add_stock(arguments)
 	
 	response_headers = [('Content-Type', 'application/json')]
 	start_response(status, response_headers)
